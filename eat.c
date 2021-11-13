2c9d8aa9525b26e1ed14450bb76b11941b9ed1b2
半田隆太 <handaryuuta@handaryuutanoMacBook-Air.local>
2021-11-13 23:42:04 +0900

    eat_countを追加

--- eat.c
+++ eat.c
@@ -6,7 +6,7 @@
 /*   By: hryuuta <hryuuta@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 17:35:05 by hryuuta           #+#    #+#             */
-/*   Updated: 2021/11/10 06:48:21 by hryuuta          ###   ########.fr       */
+/*   Updated: 2021/11/12 16:00:19 by hryuuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,17 @@ void	eat(t_philos *philo)
 	pthread_mutex_lock(&philo->mutex);
 	philo->t_last_meal = get_time();
 	put_message(philo->t_last_meal, philo->id, TYPE_EAT);
-	philo->is_eat = 1;
-	usleep(philo->info->eat_time);
-	philo->is_eat = 0;
-	pthread_mutex_lock(&philo->info->meal_check);
-	philo->info->eat_num += 1;
-	pthread_mutex_unlock(&philo->info->meal_check);
+	usleep(philo->info->eat_time * 1000);
+	philo->ate_count += 1;
+	printf("id = %d, philo->ate_count = %d\n", philo->id, philo->ate_count);
+	//printf("ate_num = %d\n", philo->info->ate_num);
+	//printf("info->philo_num = %d\n", philo->info->philo_num);
+	if (philo->ate_count == philo->info->ate_num)
+	{
+		printf("philo->ate_count = %d\n", philo->ate_count);
+		pthread_mutex_lock(&philo->info->meal_check);
+		philo->info->ate += 1;
+		pthread_mutex_unlock(&philo->info->meal_check);
+	}
 	pthread_mutex_unlock(&philo->mutex);
 }
