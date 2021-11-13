eea275c1c78f1f96da1af2920a27897bb7219a5a
半田隆太 <handaryuuta@handaryuutanoMacBook-Air.local>
2021-11-14 02:51:39 +0900

    think

--- think_and_sleep.c
+++ think_and_sleep.c
@@ -6,7 +6,7 @@
 /*   By: hryuuta <hryuuta@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 06:01:46 by hryuuta           #+#    #+#             */
-/*   Updated: 2021/11/10 06:01:57 by hryuuta          ###   ########.fr       */
+/*   Updated: 2021/11/11 17:32:29 by hryuuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	philo_sleep(t_philos *philo)
 {
-	usleep(philo->info->sleep_time);
 	put_message(get_time(), philo->id, TYPE_SLEEP);
+	usleep(philo->info->sleep_time * 1000);
 }
 
 void	think(t_philos *philo)
