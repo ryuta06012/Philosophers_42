f5ec47070e78b87665d88d82cca76ca2f06aa5a3
半田隆太 <handaryuuta@handaryuutanoMacBook-Air.local>
2021-11-26 11:10:26 +0900

    INT_MAX以上の対応を変更

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: hryuuta <hryuuta@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 09:40:44 by yootaki           #+#    #+#             */
-/*   Updated: 2021/11/22 13:09:16 by hryuuta          ###   ########.fr       */
+/*   Updated: 2021/11/25 13:38:40 by hryuuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,47 +15,49 @@
 #include <stdlib.h>
 #include <limits.h>
 
-static int	ft_ovcheck(int sign, long num, long next_num)
+static	long	over_num(int sign)
 {
-	long	ov_div;
-	long	ov_mod;
-
-	ov_div = LONG_MAX / 10;
-	ov_mod = LONG_MAX % 10;
-	if (sign == 1 && ((num > ov_div)
-			|| (ov_div == num && next_num > ov_mod)))
+	if (sign == 1)
 		return (-1);
-	if (sign == -1 && ((num > ov_div)
-			|| (ov_div == num && next_num > ov_mod + 1)))
+	else
 		return (0);
-	return (num);
 }
 
-int	ft_atoi(const char *str)
+static	int	is_step(char *str)
 {
-	int		i;
-	int		sign;
-	int		check;
-	long	result;
+	int	i;
 
-	sign = 1;
-	result = 0;
 	i = 0;
-	while (str[i] == ' ' || (str[i] >= 7 && str[i] <= 13))
+	while ((str[i] == ' ') || (str[i] == '\t') || (str[i] == '\n') || \
+			(str[i] == '\v') || (str[i] == '\r') || (str[i] == '\f'))
 		i++;
-	if (str[i] == '-')
+	return (i);
+}
+
+int	ft_atoi(char *str)
+{
+	int		sign;
+	int		i;
+	long	res;
+	long	int_div;
+	long	int_mod;
+
+	res = 0;
+	sign = 1;
+	int_div = INT_MAX / 10;
+	int_mod = INT_MAX % 10;
+	i = 0;
+	i = is_step(str);
+	if (str[i] != '\0' && (str[i] == '+' || str[i] == '-'))
 	{
-		sign = -1;
-		i++;
+		if (str[i++] == '-')
+			sign = -1;
 	}
-	else if (str[i] == '+')
-		i++;
-	while (str[i] != '\0' && (str[i] >= '0' && str[i] <= '9'))
+	while ((str[i] != '\0') && (str[i] >= '0') && (str[i] <= '9'))
 	{
-		check = ft_ovcheck(sign, result, str[i] - '0');
-		if (result > 0 && (check == 0 || check == -1))
-			return (check);
-		result = result * 10 + (str[i++] - '0');
+		if (res > int_div || (res == int_div && str[i] - '0' > int_mod))
+			return (over_num(sign));
+		res = res * 10 + str[i++] - '0';
 	}
-	return ((int)(result * sign));
+	return (res * sign);
 }
